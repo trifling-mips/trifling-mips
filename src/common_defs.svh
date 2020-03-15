b24568cd15c55d55323abcc791177e56001dbe47
Fassial <2508440340@qq.com>
2020-03-16 00:17:16 +0800

    src: divide axi3_if into axi3_rd_if & axi3_wr_if

--- src/common_defs.svh
+++ src/common_defs.svh
@@ -20,79 +20,101 @@ typedef uint32_t		phys_t;
 `define AXI3_IF_EN
 
 `ifdef AXI3_IF_EN
-// interface for AXI3
-interface axi3_if();
+// interface for AXI3 read
+interface #(
+	parameter	BUS_WIDTH	=	4
+) axi3_rd_if (
+);
 	// ar
-	uint32_t		araddr;
-	logic	[3 :0]	arlen;
-	logic	[2 :0]	arsize;
-	logic	[1 :0]	arburst;
-	logic			arlock;
-	logic	[3 :0]	arcache;
-	logic	[2 :0]	arprot;
-	logic			arvalid;
-	logic			arready;
+	logic	[BUS_WIDTH - 1:0]	arid;
+	uint32_t					araddr;
+	logic	[3 :0]				arlen;
+	logic	[2 :0]				arsize;
+	logic	[1 :0]				arburst;
+	logic						arlock;
+	logic	[3 :0]				arcache;
+	logic	[2 :0]				arprot;
+	logic						arvalid;
+	logic						arready;
 	// r
-	logic			rready;
-	uint32_t		rdata;
-	logic	[1 :0]	rresp;
-	logic			rlast;
-	logic			rvalid;
-	// aw
-	uint32_t		awaddr;
-	logic	[3 :0]	awlen;
-	logic	[2 :0]	awsize;
-	logic	[1 :0]	awburst;
-	logic			awlock;
-	logic	[3 :0]	awcache;
-	logic	[2 :0]	awprot;
-	logic			awvalid;
-	logic			awready;
-	// w
-	uint32_t		wdata;
-	logic	[3 :0]	wstrb;
-	logic			wlast;
-	logic			wvalid;
-	logic			wready;
-	// b
-	logic			bready;
-	logic	[1 :0]	bresp;
-	logic			bvalid;
+	logic	[BUS_WIDTH - 1:0]	rid;
+	logic						rready;
+	uint32_t					rdata;
+	logic	[1 :0]				rresp;
+	logic						rlast;
+	logic						rvalid;
 
 	modport master (
 		// ar
-		output araddr, arlen, arsize, arburst, arlock, arcache, arprot, arvalid;
+		output arid, araddr, arlen, arsize, arburst, arlock, arcache, arprot, arvalid;
 		input arready;
 		// r
 		output rready;
-		input rdata, rresp, rlast, rvalid;
-		// aw
-		output awaddr, awlen, awsize, awburst, awlock, awcache, awprot, awvalid;
-		input awready;
-		// w
-		output wdata, wstrb, wlast, wvalid;
-		input wready;
-		// b
-		output bready;
-		input bresp, bvalid;
+		input rid, rdata, rresp, rlast, rvalid;
 	);
 
 	modport slave (
 		// ar
-		input araddr, arlen, arsize, arburst, arlock, arcache, arprot, arvalid;
+		input arid, araddr, arlen, arsize, arburst, arlock, arcache, arprot, arvalid;
 		output arready;
 		// r
 		input rready;
-		output rdata, rresp, rlast, rvalid;
+		output rid, rdata, rresp, rlast, rvalid;
+	);
+
+endinterface
+
+// interface for AXI3 write
+interface #(
+	parameter	BUS_WIDTH	=	4
+) axi3_wr_if (
+);
+	// aw
+	logic	[BUS_WIDTH - 1:0]	awid;
+	uint32_t					awaddr;
+	logic	[3 :0]				awlen;
+	logic	[2 :0]				awsize;
+	logic	[1 :0]				awburst;
+	logic						awlock;
+	logic	[3 :0]				awcache;
+	logic	[2 :0]				awprot;
+	logic						awvalid;
+	logic						awready;
+	// w
+	logic	[BUS_WIDTH - 1:0]	wid;
+	uint32_t					wdata;
+	logic	[3 :0]				wstrb;
+	logic						wlast;
+	logic						wvalid;
+	logic						wready;
+	// b
+	logic	[BUS_WIDTH - 1:0]	bid;
+	logic						bready;
+	logic	[1 :0]				bresp;
+	logic						bvalid;
+
+	modport master (
 		// aw
-		input awaddr, awlen, awsize, awburst, awlock, awcache, awprot, awvalid;
+		output awid, awaddr, awlen, awsize, awburst, awlock, awcache, awprot, awvalid;
+		input awready;
+		// w
+		output wid, wdata, wstrb, wlast, wvalid;
+		input wready;
+		// b
+		output bready;
+		input bid, bresp, bvalid;
+	);
+
+	modport slave (
+		// aw
+		input awid, awaddr, awlen, awsize, awburst, awlock, awcache, awprot, awvalid;
 		output awready;
 		// w
-		input wdata, wstrb, wlast, wvalid;
+		input wid, wdata, wstrb, wlast, wvalid;
 		output wready;
 		// b
 		input bready;
-		output bresp, bvalid;
+		output bid, bresp, bvalid;
 	);
 
 endinterface
