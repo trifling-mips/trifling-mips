9ce315e2827d953d569ff58e5baaf04f67c64a1b
Fassial <2508440340@qq.com>
2020-03-17 01:16:28 +0800

    src: add define_func & define_struct in cache_defs.svh

--- src/cache/cache_defs.svh
+++ src/cache/cache_defs.svh
@@ -8,32 +8,24 @@
 // common defs
 `include "common_defs.svh"
 
-// define for parameter
-`define DATA_WIDTH			DATA_WIDTH
-`define TAG_WIDTH			TAG_WIDTH
-`define INDEX_WIDTH			INDEX_WIDTH
-`define DATA_PER_LINE		DATA_PER_LINE
-`define LINE_BYTE_OFFSET	LINE_BYTE_OFFSET
-`define DATA_BYTE_OFFSET	DATA_BYTE_OFFSET
-
 // data struct
-typedef struct packed {
-	logic valid;
-	logic [`TAG_WIDTH - 1:0] tag;
+`define DEF_STRUCT_TAG_T typedef struct packed { \
+	logic valid; \
+	logic [TAG_WIDTH - 1:0] tag; \
 } tag_t;
-typedef logic [`DATA_PER_LINE - 1:0][`DATA_WIDTH - 1:0] line_t;
-typedef logic [`INDEX_WIDTH - 1:0] index_t;
-typedef logic [`LINE_BYTE_OFFSET - `DATA_BYTE_OFFSET - 1:0] offset_t;
+`define DEF_STRUCT_LINE_T typedef logic [DATA_PER_LINE - 1:0][DATA_WIDTH - 1:0] line_t;
+`define DEF_STRUCT_INDEX_T typedef logic [INDEX_WIDTH - 1:0] index_t;
+`define DEF_STRUCT_OFFSET_T typedef logic [LINE_BYTE_OFFSET - DATA_BYTE_OFFSET - 1:0] offset_t;
 
 // func
-function index_t get_index( input phys_t addr );
-	return addr[`LINE_BYTE_OFFSET + `INDEX_WIDTH - 1 : `LINE_BYTE_OFFSET];
+`define DEF_FUNC_GET_INDEX function index_t get_index( input phys_t addr ); \
+	return addr[LINE_BYTE_OFFSET + INDEX_WIDTH - 1 : LINE_BYTE_OFFSET]; \
 endfunction
-function logic [`TAG_WIDTH - 1:0] get_tag( input phys_t addr );
-	return addr[31 : `LINE_BYTE_OFFSET + `INDEX_WIDTH];
+`define DEF_FUNC_GET_TAG function logic [TAG_WIDTH - 1:0] get_tag( input phys_t addr ); \
+	return addr[31 : LINE_BYTE_OFFSET + INDEX_WIDTH]; \
 endfunction
-function offset_t get_offset( input phys_t addr );
-	return addr[`LINE_BYTE_OFFSET - 1 : `DATA_BYTE_OFFSET];
+`define DEF_FUNC_GET_OFFSET function offset_t get_offset( input phys_t addr ); \
+	return addr[LINE_BYTE_OFFSET - 1 : DATA_BYTE_OFFSET]; \
 endfunction
 
 `endif
