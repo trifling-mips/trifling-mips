0a535e5d3524236c1bb25f6807fbff6eecaa57b4
Fassial <2508440340@qq.com>
2020-04-04 02:46:29 +0800

    testbench: record expr-result in cache/dcache, build pass

--- testbench/cache/dcache/test_dcache.sv
+++ testbench/cache/dcache/test_dcache.sv
@@ -203,6 +203,7 @@ endtask
 initial begin
 	wait(rst == 1'b0);
 	summary = "";
+	// can only unittest one situation, for mem_device will hold mem during initial
 	// unittest("test_inv", 6, 1);
 	// unittest("mem_bitcount", 3800, 0);
 	// unittest("mem_bubble_sort", 61613, 0);
@@ -210,20 +211,90 @@ initial begin
 	// unittest("mem_quick_sort", 38517, 0);
 	// unittest("mem_select_sort", 21594, 0);
 	// unittest("mem_stream_copy", 39924, 0);
-	unittest("mem_string_search", 33101, 0);
+	// unittest("mem_string_search", 33101, 0);
 	// unittest("random.2", 50000, 0);
 	// unittest("random.be", 50000, 1);
 	// unittest("random", 50000, 0);
 	// unittest("sequential", 32768, 0);
-	// unittest("simple", 10, 0);
+	unittest("simple", 10, 0);
 	$display("summary: %0s", summary);
 	$stop;
 end
 
 // expr result
+// test_inv
+//   [pass]
+//     Stall count:           7
+//     Read count:           2
+//     Write count:           1
+//   [OK] test_inv
+//   summary: test_inv: cycle =         171
+// mem_bitcount
+//   [pass]
+//     Stall count:          47
+//     Read count:          34
+//     Write count:           0
+//   [OK] mem_bitcount
+//   summary: mem_bitcount: cycle =        4166
+// mem_bubble_sort
+//   [pass]
+//     Stall count:         174
+//     Read count:         118
+//     Write count:           0
+//   [OK] mem_bubble_sort
+//   summary: mem_bubble_sort: cycle =       62456
+// mem_dc_coremark
+//   [pass]
+//     Stall count:         142
+//     Read count:         109
+//     Write count:           0
+//   [OK] mem_dc_coremark
+//   summary: mem_dc_coremark: cycle =       83677
+// mem_quick_sort
+//   [pass]
+//     Stall count:         778
+//     Read count:         525
+//     Write count:           0
+//   [OK] mem_quick_sort
+//   summary: mem_quick_sort: cycle =       41704
+// mem_select_sort
+//   [pass]
+//     Stall count:         174
+//     Read count:         118
+//     Write count:           0
+//   [OK] mem_select_sort
+//   summary: mem_select_sort: cycle =       22437
+// mem_stream_copy
+//   [pass]
+//     Stall count:         165
+//     Read count:          91
+//     Write count:           0
+//   [OK] mem_stream_copy
+//   summary: mem_stream_copy: cycle =       40315
+// mem_string_search
+//   [pass]
+//     Stall count:         303
+//     Read count:         579
+//     Write count:           0
+//   [OK] mem_string_search
+//   summary: mem_string_search: cycle =       34525
+// random.2
+//   [pass]
+//     Stall count:       20297
+//     Read count:        6150
+//     Write count:        3932
+//   [OK] random.2
+//   summary: random.2: cycle =      161979
+// random.be
+//   [pass]
+//     Stall count:       20548
+//     Read count:        6198
+//     Write count:        3951
+//   [OK] random.be
+//   summary: random.be: cycle =      162786
 // random
 //   [pass]
-//     Stall count:       13770
+//     Stall count:       20805
 //     Read count:        6257
 //     Write count:        4005
 //   [OK] random
