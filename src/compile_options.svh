9547d0ce09128121375e9f5bbc6cd266142df3ab
Fassial <2508440340@qq.com>
2020-04-27 21:00:07 +0800

    src: add compile option for cache in compile_options.svh

--- src/compile_options.svh
+++ src/compile_options.svh
@@ -7,7 +7,7 @@
 **/
 
 // enable all func unit
-`define COMPILE_FULL_M
+// `define COMPILE_FULL_M
 
 `ifdef COMPILE_FULL_M
     `define COMPILE_FULL    1
@@ -15,13 +15,27 @@
     `define COMPILE_FULL    0
 `endif
 
-`define CPU_MMU_ENABLED    `COMPILE_FULL 
+`define CPU_MMU_ENABLED     `COMPILE_FULL
+
+`ifdef COMPILE_FULL_M
+    `define FPU_ENABLED
+    `define ASIC_ENABLED
+`endif
 
 // can change value
 // num of tlb entries
 `define N_TLB_ENTRIES   32
+// num of regs in rf
+`define N_REG           32
 // whether enable victim cache in write_buffer
 `define VICTIM_CACHE_ENABLED    1
+// cache parameter
+`define ICACHE_LINE_WIDTH    256
+`define ICACHE_SET_ASSOC     4
+`define ICACHE_SIZE          16 * 1024 * 8
+`define DCACHE_LINE_WIDTH    256
+`define DCACHE_SET_ASSOC     2
+`define DCACHE_SIZE          8 * 1024 * 8
 
 // can undefine
 
